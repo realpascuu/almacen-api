2219ee1f6551afb1da7ae53c07f79fd6d922d1b2
Dario Guerrero <dgm94@alu.ua.es>
2023-01-25 15:32:46 +0100

    ArticuloController 1

---
+++ Controllers/ArticuloController.cs
@@ -0,0 +1,50 @@
+using almacenAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SQLite;
+using System.Data;
+using System.Text.Json;
+
+namespace almacenAPI.Controllers;
+[ApiController]
+[Route("api/articulos")]
+public class ArticulosController : ControllerBase
+{
+    private readonly ApplicationDbContext context;
+    
+    public ArticulosController(ApplicationDbContext _context)
+    {
+        this.context = _context;
+    }
+
+    [HttpGet]
+    public List<Articulo> Get()
+    {
+        var articulos = new List<Articulo>();
+        using (SQLiteConnection connection = new SQLiteConnection("Data Source= db\\Almacen.db"))
+        {
+            connection.Open();
+           
+            using (var command = new SQLiteCommand("SELECT * FROM articulo", connection))
+            {
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        articulos.Add(new Articulo
+                        {
+                            cod = Convert.ToInt32(reader["cod"]),
+                            nombre = Convert.ToString(reader["nombre"]),
+                            pvp = Convert.ToInt32(reader["pvp"]),
+                            imagen = Convert.ToString(reader["imagen"])
+                            
+                            
+                        });
+                    }
+                }
+            }
+            
+        }
+            return articulos;
+    }
+}
